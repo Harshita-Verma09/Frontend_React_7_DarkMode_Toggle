9161986f604a6353cd42845a6abc21b4ef4b7550
Harshita Verma <soniharshita0908@gmail.com>
2025-03-01 14:21:52 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Toggle from "./components/Toggle"
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <Toggle />
+      </div>
+    </>
+  )
+};
+
+export default App
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Toggle = () => {
+  const [isDark, setIsDark] = useState(false); // Track mode (light/dark)
+
+  const handleChange = () => {
+    setIsDark((prevMode) => !prevMode); // Toggle state
+  };
+
+  return (
+    <div
+      className="flex flex-col justify-center items-center min-h-screen w-full"
+      style={{
+        backgroundColor: isDark ? "black" : "white",
+        color: isDark ? "white" : "black",
+        transition: "all 0.3s ease-in-out",
+      }}
+    >
+      <div className="bg-gray-300 p-8 rounded-md">
+        <h1>Hello React Js.</h1>
+      </div>
+      <div className="flex justify-center items-center m-2 rounded-md">
+        <button
+          className="bg-blue-400 p-2 text-2xl text-white rounded-md"
+          onClick={handleChange} // ✅ Correct way to call function
+        >
+          Toggle Dark Mode
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Toggle;
